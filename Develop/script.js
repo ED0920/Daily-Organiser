37e5cea2b2081f0a139475482414c150de2f99f7
BhatJ <jisha2209@gmail.com>
2023-01-03 11:21:28 +1100

    Added weather feature
    This version retrieves weather for Sydney only

--- Develop/script.js
+++ Develop/script.js
@@ -30,4 +30,36 @@ $(function(){
         document.getElementById("hour-"+i).style.backgroundColor = '#38618c'
       }
     }
-    });
+
+    // get weather
+    getLocalWeather();
+
+});
+
+// function to get the current weather    
+var getLocalWeather = function () {
+
+  // Base URL to get icons
+  const weatherAPIIconBaseUrl = 'https://openweathermap.org/img/wn/';
+
+  // URL to retrieve weather. Currently set to get Sydney weather.
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2ceea9234e7f11c55707a7299e43d1c6';
+
+  // Request current Sydney weather
+  fetch(apiUrl) 
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          // Create selectors to weather discription, temperature and icon elements
+          var weatherDescriptionEl = $('#weather-description')[0];
+          var weatherTempEl = $('#weather-temp')[0];
+          var weatherIconEl = $('#weather-icon')[0];
+
+          // Update the description, temperature, and icon using the fetched weather data
+          // The icon is downloaded from the openweathermap host
+          weatherDescriptionEl.innerHTML = data.weather[0].description;
+          weatherTempEl.innerHTML = (data.main.temp/10.0).toFixed(2)+'°C';
+          weatherIconEl.src = weatherAPIIconBaseUrl + data.weather[0].icon + '.png';
+      });
+};
